6c83c2e68fab1bfb62ff68b5aa7a9650ff68bf36
Matthew James <matthewsalgorithm@gmail.com>
2025-10-10 16:35:58 +1100

    Not compatible with Windows.

--- clear/src/main.rs
+++ clear/src/main.rs
@@ -1,8 +1,3 @@
-use std::env;
-
 fn main() {
-    match env::var("TERM").unwrap_or_default().as_str() {
-        "xterm" | "color" => print!("\x1B[2J\x1B[1;1H"),
-        _ => {}
-    };
+    print!("\x1B[2J\x1B[1;1H");
 }
