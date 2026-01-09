8edca84bf85d432797dede2f6d51cdb0d784b810
Matthew James <matthewsalgorithm@gmail.com>
2026-01-09 23:47:22 +1100

    #18 - Add the `backup` flag with support for version control
    This includes support for the `VERSION_CONTROL` environment variable

--- mv/src/main.rs
+++ mv/src/main.rs
@@ -10,7 +10,18 @@ use std::{
     path::PathBuf
 };
 
+#[derive(PartialEq)]
+enum MVBackup {
+    None,
+    Numbered,
+    Existing,
+    Simple
+}
+
 struct MVOpts {
+    backup: bool,
+    backup_control: MVBackup,
+    backup_suffix: String,
     debug: bool,
     interactive: bool,
     no_clobber: bool,
@@ -39,6 +50,109 @@ fn prompt(prompt: &str) -> bool {
     return match user_input.to_lowercase().as_str() {
         "y" | "ye" | "yes" => true,
         _ => false
+    };
+}
+
+fn backup(path: &PathBuf, options: &MVOpts) -> Option<String> {
+    if path.is_dir() {
+        return None;
+    }
+
+    let original_file = match path.file_name() {
+        Some(name) => name.to_string_lossy(),
+        None => return None
+    };
+    
+    let parent = match path.parent() {
+        Some(parent) => parent,
+        None => return None
+    };
+
+    match options.backup_control {
+        MVBackup::None => return None,
+        MVBackup::Simple => {
+            let new_name = format!("{}{}", original_file, options.backup_suffix);
+            let mut backup_file = parent.to_path_buf();
+            backup_file.push(&new_name);
+            match fs::rename(path, &backup_file) {
+                Ok(_) => return Some(new_name),
+                Err(_) => match fs::copy(path, &backup_file) {
+                    Ok(_) => return Some(new_name),
+                    Err(_) => return None
+                }
+            }
+        }
+        MVBackup::Existing | MVBackup::Numbered => {
+            let files = match fs::read_dir(parent) {
+                Ok(entries) => entries,
+                Err(_) => return None
+            };
+
+            let numbered_prefix = format!("{}.~", original_file);
+            let numbered_suffix = "~".to_string();
+
+            let simple_name = format!("{}{}", original_file, options.backup_suffix);
+
+            let mut latest_backup: i64 = 0;
+            let mut found_simple: bool = false;
+            for entry in files {
+                match entry {
+                    Ok(entry) => {
+                        let entry_path = entry.path();
+                        let filename = match entry_path.file_name() {
+                            Some(name) => name.to_string_lossy(),
+                            None => continue
+                        };
+                        match filename.strip_prefix(&numbered_prefix) {
+                            Some(filename) => {
+                                match filename.strip_suffix(&numbered_suffix) {
+                                    Some(filename) => {
+                                        if filename.len() == 0 {
+                                            continue;
+                                        }
+                                        let n: i64 = match filename.parse() {
+                                            Ok(n) => n,
+                                            Err(_) => continue
+                                        };
+                                        
+                                        if n > latest_backup {
+                                            latest_backup = n;
+                                        }
+                                    },
+                                    None => continue
+                                }
+                            },
+                            None => {
+                                if filename == simple_name {
+                                    found_simple = true;
+                                }
+                            }
+                        }
+                    },
+                    Err(_) => continue
+                }
+            }
+
+            let mut backup_file = parent.to_path_buf();
+            if options.backup_control == MVBackup::Numbered || latest_backup != 0 || found_simple {
+                backup_file.push(format!("{}.~{}~", original_file, latest_backup + 1));
+            } else {
+                backup_file.push(format!("{}{}", original_file, options.backup_suffix));
+            }
+
+            let new_name = match backup_file.file_name() {
+                Some(name) => name.to_string_lossy().to_string(),
+                None => return None
+            };
+
+            match fs::rename(path, &backup_file) {
+                Ok(_) => return Some(new_name),
+                Err(_) => match fs::copy(path, &backup_file) {
+                    Ok(_) => return Some(new_name),
+                    Err(_) => return None
+                }
+            }
+        }
     };
 }
 
@@ -93,7 +207,18 @@ fn mv(options: &MVOpts, paths: &Vec<PathBuf>) {
             } else if options.no_clobber {
                 continue;
             }
+
+            if options.backup {
+                if let Some(backup_file) = backup(destination_path, options) {
+                    if options.verbose {
+                        println!("renamed '{}' -> '{}'",
+                            path.to_string_lossy(),
+                            backup_file);
+                    }
+                }
+            }
         }
+
         if !crosses_devices {
             match fs::rename(path, destination_path) {
                 Ok(_) => {
@@ -102,6 +227,7 @@ fn mv(options: &MVOpts, paths: &Vec<PathBuf>) {
                             path.to_string_lossy(),
                             destination_path.to_string_lossy());
                     }
+                    continue;
                 },
                 Err(err) => match err.kind() {
                     ErrorKind::CrossesDevices => crosses_devices = true,
@@ -109,6 +235,7 @@ fn mv(options: &MVOpts, paths: &Vec<PathBuf>) {
                 }
             }
         }
+
         match fs::copy(path, destination_path) {
             Ok(_) => {
                 if options.debug {
@@ -136,6 +263,9 @@ fn mv(options: &MVOpts, paths: &Vec<PathBuf>) {
 
 fn main() {
     let mut options: MVOpts = MVOpts {
+        backup: false,
+        backup_control: MVBackup::Existing,
+        backup_suffix: "~".to_string(),
         debug: false,
         interactive: true,
         no_clobber: false,
@@ -147,6 +277,12 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     let mut expanded_args: Vec<String> = Vec::new();
+
+    match env::var("VERSION_CONTROL") {
+        Ok(version) => expanded_args.push(format!("--backup={}", version)),
+        Err(_) => {}
+    }
+
     for arg in args.iter().skip(1) {
         if arg.len() > 2 && arg.starts_with('-') && !arg.starts_with("--") {
             for ch in arg.chars().skip(1) {
@@ -156,8 +292,10 @@ fn main() {
             expanded_args.push(arg.to_string());
         }
     }
+
     for arg in expanded_args {
         match arg.as_str() {
+            "-b" | "--backup" => options.backup = true,
             "--debug" => {
                 options.debug = true;
                 options.verbose = true;
@@ -167,6 +305,22 @@ fn main() {
             "-n" | "--no-clobber" => options.no_clobber = true,
             "--strip-trailing-slashes" => options.strip_trailing_slashes = true,
             "--verbose" => options.verbose = true,
+            arg if arg.starts_with("--backup=") => {
+                let version = match arg.strip_prefix("--backup=") {
+                    Some(version) => version.to_lowercase(),
+                    None => continue
+                };
+                match version.as_str() {
+                    "t" | "numbered" => options.backup_control = MVBackup::Numbered,
+                    "nil" | "existing" => options.backup_control = MVBackup::Existing,
+                    "never" | "simple" => options.backup_control = MVBackup::Simple,
+                    "none" | "off" => options.backup_control = MVBackup::None,
+                    _ => {
+                        println!("\x1b[0;91mError: Unknown version control '{}'.\x1b[0m", arg);
+                        return;
+                    }
+                }
+            },
             arg if !arg.starts_with('-') && !arg.starts_with("--") =>
                 paths.push(PathBuf::from(arg)),
             _ => {
