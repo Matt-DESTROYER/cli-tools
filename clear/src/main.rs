6108f9b6d9ce4fac07a428f2a42ca59bfb2947e9
Matthew James <matthewsalgorithm@gmail.com>
2026-01-10 12:53:56 +1100

    #19 - On Windows just call `cls` and fallback to Unix based print if fails

--- clear/src/main.rs
+++ clear/src/main.rs
@@ -1,3 +1,15 @@
+#[cfg(windows)]
+use std::process::Command;
+
 fn main() {
+    #[cfg(windows)] {
+        match Command::new("cmd")
+                .args(["/c", "cls"])
+                .status() {
+            Ok(_) => return,
+            Err(_) => {}
+        }
+    }
+    #[cfg(not(windows))]
     print!("\x1B[2J\x1B[1;1H");
 }
